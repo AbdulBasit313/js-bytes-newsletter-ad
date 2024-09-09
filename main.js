f47d6e9342046d30fa4228e83e2cb7f42e1bcaff
Abdul Basit <abdul_basit313@outlook.com>
2024-09-09 16:09:20 +0500

    improve style

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-function renderNewsletterAdCard() {
-  // Create the container div
-  const adContainer = document.createElement("div");
-  adContainer.className = "ad-container";
+import "./style.css";
 
-  // Add the inner HTML content
-  adContainer.innerHTML = `
-      <a href="https://codevertiser.substack.com/" target="_blank">
+document.querySelector("#app").innerHTML = `
+  <div class="ad-container">
+      <a href="https://codevertiser.substack.com/" target="_blank" class="ad-container-title">
         <h6>
           Become a better JavaScript developer by practicing only 10–20 minutes a week
         </h6>
@@ -17,16 +14,8 @@ function renderNewsletterAdCard() {
           <li>Three conceptual JavaScript MCQs</li>
         </ul>
       </div>
-      <a href="https://codevertiser.substack.com/" target="_blank">
+      <a href="https://codevertiser.substack.com/" target="_blank" class="ad-container-btn">
         Subscribe to JS Bytes
       </a>
+      </div>
     `;
-
-  // Append to the target container on the page
-  const targetElement = document.getElementById("ad-container");
-  if (targetElement) {
-    targetElement.appendChild(adContainer);
-  }
-}
-
-// renderNewsletterAdCard();
